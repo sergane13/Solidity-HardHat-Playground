21d53207c243163661a37c2187eea05938de75e3
serban <serban13chisca@gmail.com>
2022-01-07 10:54:42 -0500

    writing code

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -4,7 +4,42 @@ pragma solidity 0.8.11;
 
 contract MultiSigWallet
 {
-    mapping(address => uint) value ; 
+    // array with owners of the wallet
+    address[] private owners; 
+
+    // store in a mapping with the value bools, if an address is an owner
+    mapping(address => bool) private checkOwners;
+
+    // struct of transaction
+    struct Transaction
+    {
+        address recipient;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 numberOfConfirmations;
+    }
+
+    // index of transaction
+    uint256 private transactionIndex;
+
+    // array with all transactions done by this contract    
+    Transaction[] private allTransactions;
+    
+    /*
+        mapping to know who approved or not a transaction
+        uint -> transaction index
+        address -> address of the owner
+        bool -> if it has been approved by the owner
+    */
+    mapping(uint256 => mapping(address => bool)) private transactionApprovals;
+
+
+    constructor(address[] memory _owners) 
+    {   
+        
+    }
+
 
     // constructor assign the owners of the contract addresses
     // mapping to store owners of the contract as bool
