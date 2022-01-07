4cdfc5d65c473c0539e1a1092e0c656d7aa1268d
serban <serban13chisca@gmail.com>
2022-01-07 10:02:07 -0500

    new branch

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -6,11 +6,19 @@ contract MultiSigWallet
 {
     mapping(address => uint) value ; 
 
+    // constructor assign the owners of the contract addresses
+    // mapping to store owners of the contract as bool
+    // mimum required votes
+
     // onlyOwners can submit verifications
-    // Array of transactions
+
+    // mapping with txid => address => bool to check if the transaction members approved 
+    // Array of transactions done
+    // transaction index
  
     // Submit transaction to be approved
     // Aprove transaction existend
     // Execute transaction execute transaction if mimum requirments are done
     // Revoke approvement from a transaction 
+    // Revoke transaction from execution
 }
